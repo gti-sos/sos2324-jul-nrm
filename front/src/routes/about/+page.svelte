9b9f8f60c1d769ee4a1806c0b60102c1f81ba135
NicoRedondoo <redondonico9@gmail.com>
2024-07-04 13:03:36 +0200

    D03

---
+++ front/src/routes/about/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { dev } from "$app/environment";
+    import { Container } from '@sveltestrap/sveltestrap';
+
+    let mode = "production"
+
+    if(dev)
+        mode = "development"
+</script>
+
+<Container>
+    Estas en modo {mode}
+	<h1>Vídeos</h1>
+	<ul>
+		<li><a href="https://www.youtube.com/embed/vJf16rV6YBI?si=1q9xEKCPqKYE8j90">Video Víctor Escalera García</a></li>
+        <li><a href="https://www.loom.com/share/a266187220b04de093b1dd673d028c94">Nicolás Redondo Moreno</a></li>
+        <li><a href="https://youtu.be/VNjWoELJL7E">Enrique García Olivares</a></li>
+	</ul>
+</Container>
