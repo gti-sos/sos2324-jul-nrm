524404b634f99e8e0ed72c7df9772a07e6aae375
NicoRedondoo <redondonico9@gmail.com>
2024-07-04 13:58:42 +0200

    D03

--- front/src/routes/about/+page.svelte
+++ front/src/routes/about/+page.svelte
@@ -10,10 +10,9 @@
 
 <Container>
     Estas en modo {mode}
+    <br>
 	<h1>Vídeos</h1>
 	<ul>
-		<li><a href="https://www.youtube.com/embed/vJf16rV6YBI?si=1q9xEKCPqKYE8j90">Video Víctor Escalera García</a></li>
         <li><a href="https://www.loom.com/share/a266187220b04de093b1dd673d028c94">Nicolás Redondo Moreno</a></li>
-        <li><a href="https://youtu.be/VNjWoELJL7E">Enrique García Olivares</a></li>
 	</ul>
 </Container>
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import dataStore from "nedb";
 import {handler} from "./front/build/handler.js";
-
+import cors from 'cors';
 
 //API
 import {apiUfc} from './API/api-v1.js';
@@ -11,12 +11,12 @@ let dataUfc = new dataStore();
 let app = express();
 const PORT = (process.env.PORT || 10002);
 
-//Proxy NRM
-app.use("/ufc-events-data", function(req,res){
-    var url = "https://sos2324-jul-nrm-428011.ew.r.appspot.com/api/v1/ufc-events-data";
-    console.log("piped: " + req.url);
-    req.pipe(request(url)).pipe(res);
-});
+app.use(cors({
+    "origin": "*",
+    "methods" : "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus":204
+}));
 
 app.use(bodyParser.json());
 
